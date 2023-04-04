d15b24b38ec62e95cfe146f5f90eec2654c95741
pedroojanuu <93834123+pedroojanuu@users.noreply.github.com>
2023-04-04 11:43:18 +0100

    Documented District.h and Municipality.h. Doc not generated.

--- Classes/District.h
+++ Classes/District.h
@@ -7,12 +7,33 @@
 #include "VertexEdge.h"
 
 class Station;
-
+/**
+ * @brief A class for a Portuguese district.
+ *
+ * Represents a district of Portugal by its name and includes pointers to the stations on it.
+ */
 class District {
 public:
+    /**
+     * Constructor. Receives a district name.
+     * @param name
+     */
     District(const std::string& name);
+    /**
+     * Adds a given station pointer to the district's stations.
+     * @param station
+     */
     void addStation(Station* station);
+    /**
+     * Less than (District A < District B) comparison operator. Lexicographically compares the districts' names.
+     * @param other
+     * @return True if A's name is lexicographically lower than B's name, false otherwise.
+     */
     bool operator<(const District& other) const;
+    /**
+     * District name's getter.
+     * @return The string that is the District's name.
+     */
     std::string getName() const;
 private:
     std::string name;
--- Classes/Municipality.h
+++ Classes/Municipality.h
@@ -8,11 +8,33 @@
 
 class Station;
 
+/**
+ * @brief A class for a Portuguese Municipality.
+ *
+ * Represents a municipality of Portugal by its name and includes pointers to the stations on it.
+ */
 class Municipality {
 public:
+    /**
+     * Constructor. Receives a municipality name.
+     * @param name
+     */
     Municipality(const std::string& name);
+    /**
+     * Adds a given station pointer to the municipality's stations.
+     * @param station
+     */
     void addStation(Station* station);
+    /**
+     * Less than (Municipality A < Municipality B) comparison operator. Lexicographically compares the municipalities' names.
+     * @param other
+     * @return True if A's name is lexicographically lower than B's name, false otherwise.
+     */
     bool operator<(const Municipality& other) const;
+    /**
+     * Municipality's name getter.
+     * @return The string that is the Municipality's name.
+     */
     std::string getName() const;
 private:
     std::string name;
