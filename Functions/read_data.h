ae25d3e06267d19ef4562244839a6d0f96cfcb38
pedroojanuu <93834123+pedroojanuu@users.noreply.github.com>
2023-04-04 11:23:06 +0100

    Documented read_data.h

--- Functions/read_data.h
+++ Functions/read_data.h
@@ -11,7 +11,21 @@
 #include "../Classes/Municipality.h"
 #include "../Classes/District.h"
 
+/**
+ * Reads the stations in a given csv file and populates a given graph with those stations.
+ *
+ * @param filename
+ * @param graph
+ */
 void readStations(const std::string& filename, Graph& graph);
+
+/**
+ * Reads the railway segments in a given csv file and populates a given graph with those stations.
+ * Cannot be called before readStations().
+ *
+ * @param filename
+ * @param graph
+ */
 void readNetwork(const std::string& filename, Graph& graph);
 
 #endif //DA_PROJ1_READ_DATA_H
