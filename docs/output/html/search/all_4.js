b4d7736952d85bbcd2bb41d01b1b3a9e8c3ea550
pedroojanuu <93834123+pedroojanuu@users.noreply.github.com>
2023-04-05 11:42:28 +0100

    Main page writen.

--- docs/output/html/search/all_4.js
+++ docs/output/html/search/all_4.js
@@ -1,11 +1,12 @@
 var searchData=
 [
-  ['dataready_0',['dataReady',['../class_menu.html#a06cfe73cf3a50dd7667500d4ab4b358b',1,'Menu']]],
-  ['dec_1',['DEC',['../_c_make_c_compiler_id_8c.html#ad1280362da42492bbc11aa78cbf776ad',1,'DEC():&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#ad1280362da42492bbc11aa78cbf776ad',1,'DEC():&#160;CMakeCXXCompilerId.cpp']]],
-  ['dest_2',['dest',['../class_edge.html#ae05e074c4b8345baaad6655c44f205ec',1,'Edge']]],
-  ['district_3',['District',['../class_district.html',1,'District'],['../class_district.html#a3d7298f1319e9692fd9d3f90be1153dd',1,'District::District()']]],
-  ['district_4',['district',['../class_station.html#a0c2e82f562643d835ed1e2eb6e43e7f3',1,'Station']]],
-  ['district_2ecpp_5',['District.cpp',['../_district_8cpp.html',1,'']]],
-  ['district_2eh_6',['District.h',['../_district_8h.html',1,'']]],
-  ['districtflow_7',['districtFlow',['../read__data_8cpp.html#a6b2cd7130ee68f12863fe010a542f290',1,'read_data.cpp']]]
+  ['da_3a_20programming_20project_20i_0',['DA: Programming Project I',['../index.html',1,'']]],
+  ['dataready_1',['dataReady',['../class_menu.html#a06cfe73cf3a50dd7667500d4ab4b358b',1,'Menu']]],
+  ['dec_2',['DEC',['../_c_make_c_compiler_id_8c.html#ad1280362da42492bbc11aa78cbf776ad',1,'DEC():&#160;CMakeCCompilerId.c'],['../_c_make_c_x_x_compiler_id_8cpp.html#ad1280362da42492bbc11aa78cbf776ad',1,'DEC():&#160;CMakeCXXCompilerId.cpp']]],
+  ['dest_3',['dest',['../class_edge.html#ae05e074c4b8345baaad6655c44f205ec',1,'Edge']]],
+  ['district_4',['District',['../class_district.html',1,'District'],['../class_district.html#a3d7298f1319e9692fd9d3f90be1153dd',1,'District::District()']]],
+  ['district_5',['district',['../class_station.html#a0c2e82f562643d835ed1e2eb6e43e7f3',1,'Station']]],
+  ['district_2ecpp_6',['District.cpp',['../_district_8cpp.html',1,'']]],
+  ['district_2eh_7',['District.h',['../_district_8h.html',1,'']]],
+  ['districtflow_8',['districtFlow',['../read__data_8cpp.html#a6b2cd7130ee68f12863fe010a542f290',1,'read_data.cpp']]]
 ];
--- main.cpp
+++ main.cpp
@@ -1,27 +1,24 @@
-#include <iostream>
 #include "Interface/Menu.h"
 
+/**
+ * @mainpage DA: Programming Project I
+ *
+ * This is a possible solution for the first programming project of the DA (Desenho de Algoritmos) course, which belongs
+ * to the 2nd year of the Licenciatura em Engenharia Informática e Computação, taught by Faculdade de Engenharia da
+ * Universidade do Porto (FEUP) and Faculdade de Ciências da Universidade do Porto (FCUP).
+ *
+ * This work was developed by João Francisco da Rocha Sequeira Alves (202006281), Pedro de Almeida Lima (202108806)
+ * and Pedro Simão Januário Vieira (202108768).
+ *
+ * We modelled the provided railway network as a Graph where the Stations are the Vertexes/Nodes (class Station) and the
+ * railway segments that connect pairs of Stations are the Edges (class Edge). For this purpose, we reused and adapted
+ * the Graph implementation used in classes.
+ *
+ * We developed a user interface consisting of a menu with 4 submenus, each one covering one set of tasks as described
+ * in the project handout. The UI is entirely text-based.
+ */
+
 int main() {
-    /*Graph graph;
-    readStations("Project1Data/stations.csv", graph);
-    readNetwork("Project1Data/network.csv", graph);
-
-//    for(auto d : all_districts)
-//        std::cout << d -> getName() << std::endl;
-//
-//    for(auto m : all_municipalities)
-//        std::cout << m -> getName() << std::endl;
-
-//    for(auto v : graph.getVertexSet()) {
-//        std::cout << v->getName() << ", " << v->getDistrict()->getName() << ", " << v->getMunicipality()->getName()
-//                  << ", " << v->getTownship() << ", " << v->getLine() << std::endl;
-//        for(auto e : v->getAdj())
-//            std::cout << "----- " << e->getDest()->getName() << ", " << e->getWeight() << ", " << e->isAlfa() << std::endl;
-//    }
-
-
-    graph.edmondsKarp("Porto Campanhã", "Lisboa Oriente");*/
-
     Menu menu;
     menu.mainMenu();
 
